8375bf0002567a797a06f613aa0d7dc10c96ab98
radosavb <radosav.boz@gmail.com>
2019-09-05 20:10:59 +0200

    Dodat atr. name za polja unosa i nova polja Lokacija i testiranje

--- unos_oglasa.php
+++ unos_oglasa.php
@@ -19,10 +19,6 @@
     <?php
         include('header.php');
         ?>
-        <div class="container bg-light text-dark py-3 my-3">
-            <div class="row">
-                <h2 class="px-3"></h2>
-            </div>
 
             <form action="" method="" class="">
                 <fieldset>
@@ -30,19 +26,19 @@
                     <div class="form-row mb-3">
                         <div class="form-group col-md-6">
                             <label for="naziv" class="">Naziv laptopa</label>
-                            <input type="text" id="naziv" class="form-control" placeholder="npr. Dell Inspiron 15 3580">
+                            <input type="text" id="naziv" name="naziv" class="form-control" placeholder="npr. Dell Inspiron 15 3580">
                         </div>
                         <div class="form-group col-md-3">
                             <label for="dodaj_sliku">Dodaj sliku:</label>
-                            <input type="file" id="dodaj_sliku" class="form-control-file">
+                            <input type="file" id="dodaj_sliku" name="dodaj_sliku" class="form-control-file">
                         </div>
                         <div class="form-group col-md-2 col-6">
                             <label for="cena" class="">Cena:</label>
-                            <input type="int" id="cena" class="form-control">
+                            <input type="int" id="cena" name="cena" class="form-control">
                         </div>
                         <div class="form-group col-md-1 col-6">
                             <label for="valuta">Valuta:</label>
-                            <select id="valuta" class="form-control">
+                            <select id="valuta" name="valuta" class="form-control">
                                 <option>€</option>
                                 <option>RSD</option>
                             </select>
@@ -53,7 +49,7 @@
 
                         <div class="form-group col-lg-3 col-md-6">
                             <label for="cpu">Procesor</label>
-                            <select id="cpu" class="form-control">
+                            <select id="cpu" name="cpu" class="form-control">
                                 <option value="">---</option>
                                 <option value="Intel Core i9 (osmojezgarni)">Intel Core i9 (osmojezgarni)</option>
                                 <option value="Intel Core i9 (šestojezgarni)">Intel Core i9 (šestojezgarni)</option>
@@ -75,13 +71,13 @@
                         </div>
 
                         <div class="form-group col-lg-3 col-md-6">
-                            <label for="cpu1">Opis procesora</label>
-                            <input class="form-control" type="text" id="cpu1" placeholder="npr. 1.8Ghz, 4MB cache">
+                            <label for="cpu_opis">Opis procesora</label>
+                            <input class="form-control" type="text" id="cpu_opis" name="cpu_opis" placeholder="npr. 1.8Ghz, 4MB cache">
                         </div>
 
                         <div class="form-group col-lg-3 col-md-6">
                             <label for="ram">RAM</label>
-                            <select id="ram" class="form-control">
+                            <select id="ram" name="ram" class="form-control">
                                 <option value="">---</option>
                                 <option value="1 GB">1 GB</option>
                                 <option value="2 GB">2 GB</option>
@@ -95,8 +91,8 @@
                         </div>
 
                         <div class="form-group col-lg-3 col-md-6">
-                            <label for="ram1">Opis RAM memorije</label>
-                            <input class="form-control" type="text" id="ram1" placeholder="npr. 2GB + 4GB DDR 4">
+                            <label for="tip_rama">Opis RAM memorije</label>
+                            <input class="form-control" type="text" name="tip_rama" id="tip_rama" placeholder="npr. 2GB + 4GB DDR 4">
                         </div>
 
                     </div>
@@ -105,7 +101,7 @@
 
                         <div class="form-group col-lg-3 col-md-6">
                             <label for="ekran">Ekran</label>
-                            <select id="ekran" class="form-control">
+                            <select id="ekran" name="ekran" class="form-control">
                                 <option value="">---</option>
                                 <option value="12">12"</option>
                                 <option value="14">14"</option>
@@ -114,12 +110,12 @@
                             </select>
                         </div>
                         <div class="form-group col-lg-3 col-md-6">
-                            <label for="ekran1">Opis ekrana</label>
-                            <input class="form-control" type="text" id="ekran1" placeholder="npr. matt IPS ACER antiglare">
+                            <label for="ekran_opis">Opis ekrana</label>
+                            <input class="form-control" type="text" id="ekran_opis" name="ekran_opis" placeholder="npr. matt IPS ACER antiglare">
                         </div>
                         <div class="form-group col-lg-3 col-md-6">
                             <label for="gpu">Grafička kartica:</label>
-                            <select id="gpu" class="form-control">
+                            <select id="gpu" name="gpu" class="form-control">
                                 <option value="">---</option>
                                 <option value="Integrisana">Integrisana</option>
                                 <option value="NVidia">NVidia</option>
@@ -128,8 +124,8 @@
                         </div>
 
                         <div class="form-group col-lg-3 col-md-6">
-                            <label for="gpu1">Opis GPU</label>
-                            <input class="form-control" type="text" id="gpu1" placeholder="npr. NVidia GTX 1050">
+                            <label for="gpu_opis">Opis GPU</label>
+                            <input class="form-control" type="text" id="gpu_opis" name="gpu_opis" placeholder="npr. NVidia GTX 1050">
                         </div>
 
                     </div>
@@ -138,7 +134,7 @@
 
                         <div class="form-group col-lg-3 col-md-6">
                             <label for="hdd">HDD</label>
-                            <select id="hdd" class="form-control">
+                            <select id="hdd" name="hdd" class="form-control">
                                 <option value="">---</option>
                                 <option value="HDD 112 GB">HDD 112 GB</option>
                                 <option value="HDD 256 GB">HDD 256 GB</option>
@@ -148,13 +144,13 @@
                             </select>
                         </div>
                         <div class="form-group col-lg-3 col-md-6">
-                            <label for="hdd1">Opis HDD</label>
-                            <input class="form-control" type="text" id="hdd1" placeholder="5400 obrtaja/min.">
+                            <label for="hdd_opis">Opis HDD</label>
+                            <input class="form-control" type="text" id="hdd_opis" name="hdd_opis" placeholder="npr. 5400 obrtaja/min.">
                         </div>
 
                         <div class="form-group col-lg-3 col-md-6">
                             <label for="ssd">SSD</label>
-                            <select id="ssd" class="form-control">
+                            <select id="ssd" name="ssd" class="form-control">
                                 <option value="">---</option>
                                 <option value="SSD 112 GB">SSD 112 GB</option>
                                 <option value="SSD 256 GB">SSD 256 GB</option>
@@ -164,16 +160,16 @@
                         </div>
 
                         <div class="form-group col-lg-3 col-md-6">
-                            <label for="sdd1">Opis SDD</label>
-                            <input class="form-control" type="text" id="ssd1" placeholder="npr. TOSHIBA NVMe">
+                            <label for="ssd_opis">Opis SDD</label>
+                            <input class="form-control" type="text" id="ssd_opis" name="ssd_opis" placeholder="npr. TOSHIBA NVMe">
                         </div>
 
                     </div>
-                    <div class="form-row mb-3">
+                    <div class="form-row">
 
                         <div class="form-group col-lg-3 col-md-6">
                             <label for="os">Operativni sistem</label>
-                            <select id="os" class="form-control">
+                            <select id="os" name="os" class="form-control">
                                 <option value="Nema">Nema</option>
                                 <option value="Windows">Windows</option>
                                 <option value="Linux">Linux</option>
@@ -183,22 +179,39 @@
                         <div class="form-group col-lg-3 col-md-6">
                             <label for="grupa">Garancija</label>
                             <div id="grupa" class="form-check d-inline">
-                                <input class="form-check-input" type="checkbox" id="garancija" value="ima_garanciju">
-                                <input id="meseci" class="form-control" type="int" placeholder="u mesecima">
+                                <input class="form-check-input" type="checkbox" id="garancija" name="garancija" value="da">
+                                <input id="meseci" name="meseci" class="form-control" type="int" placeholder="u mesecima">
                             </div>
                             <small>*Ukoliko je laptop još uvek pod garancijom čekirajte kvadratić</small>
                         </div>
 
                         <div class="form-group col-lg-6 col-md-12">
                             <label for="opis">Slobodan opis laptopa</label>
-                            <textarea class="form-control" id="opis" cols="30" rows="5"></textarea>
+                            <textarea class="form-control" id="opis" name="opis" cols="30" rows="5"></textarea>
                             <small>*Što detaljnije opišite stanje laptopa koji oglašavate</small>
                         </div>
 
                     </div>
 
+                    <div class="form-row mb-3">
+
+                        <div class="form-group col-md-6">
+                            <label for="lokacija">Lokacija</label>
+                            <input class="form-control" type="text" id="lokacija" name="lokacija" placeholder="npr. Beograd, Zvezdara">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="testiranje">Želim da se moj laptop testira:</label>
+                            <select id="testiranje" name="testiranje" class="form-control">
+                                <option value="da">Da</option>
+                                <option value="ne">Ne</option>
+                            </select>
+                        </div>
+
+                    </div>
+
                     <div class="row d-flex justify-content-center pb-5">
-                        <input class="btn btn-primary w-25" type="submit" value="Predaj oglas">
+                        <input class="btn btn-primary w-25" type="submit" id="predaj_oglas" name="predaj_oglas" value="Predaj oglas">
                     </div>
 
                 </fieldset>
