adb297b6797417116e4c106e1a595c20d61ad085
BoskoMilovanovic <boskobmilovanovi@gmail.com>
2019-10-20 23:44:54 +0200

    Dodati korisnik i oglas

--- Lapodorm.sql
+++ Lapodorm.sql
@@ -14,7 +14,7 @@ SET time_zone = "+01:00"; -- Definisanje vremenske zone
 /*!40101 SET NAMES utf8mb4 */;
 
 create table korisnici
-(user_id int auto_increment,
+(user_id int not null auto_increment,
 ime varchar(256),
 prezime varchar(256),
 email varchar(256),
@@ -28,7 +28,7 @@ constraint k_pk primary key (user_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 create table oglas
-(oglas_id int auto_increment,
+(oglas_id int not null auto_increment,
 naziv varchar(30),
 cena numeric(30),
 slika varchar(100),
@@ -63,6 +63,11 @@ constraint t1_fk foreign key (user_id) references korisnici (user_id),
 constraint t2_fk foreign key (oglas_id) references oglas (oglas_id),
 constraint ocena_check check (ocena IN ('1','2','3','4','5'))
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+insert into korisnici (ime,prezime,email,sifra,tel,adresa,mode) values ('Marko','Markovic','markomarkovic@hotmail.com','$2a$07$AHcTAlLDVapYECnD6xlva.RYROubO17KADs3fx.e/WF8kGOzf3z0O',060123456,'IT Heroja bb','admin');
+insert into oglas (naziv,cena,slika,cpu,cpu_opis,ram,tip_rama,gpu,gpu_opis,ekran,ekran_opis,hdd1,hdd1_opis,hdd2,hdd2_opis,os,slob_opis,lokacija,garancija,datum_oglasa,user_id) 
+values ('Lenovo Thinkpad T440p',15000,'default.png','i5','i5-5200U 2 cores 4 threads','8','DDR4','Integrisana','Intel HD5500','14','IPS ekran 1080p','1024','Toshiba 1TB sata3','256','SanDisk SSD','Nema',
+'U odlicnom stanju. Ocuvan.','Beograd',0,'2019-09-17',1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
